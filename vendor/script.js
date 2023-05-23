38fe9fc104d5804a05d87ff34a7300f0beb0175f
Roman Makhnev <namelessone154@yandex.ru>
2023-05-24 00:19:01 +0300

    Реализация первого пункта проектной работы

--- vendor/script.js
+++ vendor/script.js
@@ -1,8 +1,44 @@
-let editButton = document.querySelector('.profile__edit');
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileDesc = profile.querySelector('.profile__description');
+let profileEditBtn = profile.querySelector('.profile__edit');
+
+let card = document.querySelector('.card');
+let cardLikeBtn = card.querySelectorAll('.card__like');
+
 let popup = document.querySelector('.popup');
+let popupCloseBtn = popup.querySelector('.popup__close');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('#popup__text_name');
+const jobInput = popup.querySelector('#popup__text_status');
+
+function updateValues() {
+  nameInput.setAttribute('value', profileName.textContent);
+  jobInput.setAttribute('value', profileDesc.textContent);
+}
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  updateValues();
 }
 
-editButton.addEventListener('click', openPopup);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDesc.textContent = jobInput.value;
+  updateValues();
+  closePopup();
+}
+
+function like() {
+  cardLikeBtn.classList.add('card__like_active');
+}
+
+profileEditBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+cardLikeBtn.addEventListener('click', like);
