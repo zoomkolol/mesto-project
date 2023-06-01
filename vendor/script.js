93243e85156ec4180f947a68adb6231e310f41d4
Roman Makhnev <namelessone154@yandex.ru>
2023-06-02 01:56:25 +0300

    Added several changes to openPopup func

--- vendor/script.js
+++ vendor/script.js
@@ -2,6 +2,7 @@ let profile = document.querySelector('.profile');
 let profileName = profile.querySelector('.profile__name');
 let profileDesc = profile.querySelector('.profile__description');
 let profileEditBtn = profile.querySelector('.profile__edit');
+let profileAddBtn = profile.querySelector('profile__add');
 
 let cardsContainer = document.querySelector('.cards');
 let card = cardsContainer.querySelector('.card');
@@ -9,8 +10,14 @@ let card = cardsContainer.querySelector('.card');
 let popup = document.querySelector('.popup');
 let popupCloseBtn = popup.querySelector('.popup__close');
 const formElement = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('#popup__text_name');
-const jobInput = popup.querySelector('#popup__text_status');
+
+const profileForm = document.querySelector('form[name="edit-profile"]');
+const nameInput = popup.querySelector('input[name="name"]');
+const jobInput = popup.querySelector('input[name="status"]');
+
+const cardForm = document.querySelector('form[name="edit-card"]');
+const placeInput = popup.querySelector('input[name="place"]');
+const linkInput = popup.querySelector('input[name="link"]');
 
 const initialCards = [
   {
@@ -40,13 +47,17 @@ const initialCards = [
 ];
 
 function updateValues() {
-  nameInput.setAttribute('value', profileName.textContent);
-  jobInput.setAttribute('value', profileDesc.textContent);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDesc.textContent;
 }
 
-function openPopup() {
+function openPopup(button) {
   popup.classList.add('popup_opened');
-  updateValues();
+  if(button.target == profileEditBtn) {
+    profileForm.style.display = 'flex';
+    cardForm.style.display = 'none';
+    updateValues();
+  }
 }
 
 function closePopup() {
@@ -76,10 +87,10 @@ function addCard(cardName, cardLink) {
   cardsContainer.append(cardElement);
 }
 
-profileEditBtn.addEventListener('click', openPopup);
-popupCloseBtn.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
-
 for (let i = 0; i < initialCards.length; i++) {
   addCard(initialCards[i].name, initialCards[i].link);
 }
+
+profileEditBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
