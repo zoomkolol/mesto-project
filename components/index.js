7e370dbf7488d9bbb00a9a4d17f5e5d553472e21
Roman Makhnev <namelessone154@yandex.ru>
2023-07-02 09:34:43 +0300

    Almost done with sprint 8

--- components/index.js
+++ components/index.js
@@ -8,6 +8,9 @@ const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.cards');
 const card = cardsContainer.querySelector('.card');
 
+const popup = document.querySelectorAll('.popup');
+const popupOverlay = document.querySelectorAll('.popup__overlay');
+const popupContainer = document.querySelectorAll('.popup__container');
 const popupEditProfile = document.querySelector('.popup__edit-profile');
 const popupEditCard = document.querySelector('.popup__edit-card');
 const popupImageContainer = document.querySelector('.popup__image-container');
@@ -22,6 +25,69 @@ const jobInput = document.querySelector('input[name="status"]');
 const cardForm = document.querySelector('form[name="edit-card"]');
 const placeInput = document.querySelector('input[name="place"]');
 const linkInput = document.querySelector('input[name="link"]');
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
+  inputElement.classList.add('popup__text_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__text_error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}_error`);
+  inputElement.classList.remove('popup__text_type_error');
+  errorElement.classList.remove('popup__text_error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (form) => {
+  const inputList = Array.from(form.querySelectorAll('.popup__text'));
+  const buttonElement = form.querySelector('.popup__submit');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(form, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+   formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+   });
+   setEventListeners(formElement);
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.setAttribute('disabled', '');
+  }
+  else {
+    buttonElement.removeAttribute('disabled');
+  }
+};
+
+enableValidation();
+
 
 function updateValues() {
   nameInput.value = profileName.textContent;
@@ -46,19 +112,23 @@ function closePopup(popup) {
 }
 
 function closePopupBtn(evt) {
-  const popupToClose = evt.target.closest('.popup');
-  closePopup(popupToClose);
+  closePopup(evt.closest('.popup'));
+}
+
+function handleClosePopupKey(evt) {
+  if(evt.key === 'Escape') {
+    closePopup(evt.target.closest('.popup'));
+    console.log(true);
+  }
 }
 
 function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDesc.textContent = jobInput.value;
   closePopup(popupEditProfile);
 }
 
 function handleCardFormSubmit(evt) {
-  evt.preventDefault();
   renderCard(placeInput.value, linkInput.value);
   cardForm.reset();
   closePopup(popupEditCard);
@@ -109,9 +179,18 @@ initialCards.forEach(function(card) {
   renderCard(card.name, card.link);
 });
 
+
 profileEditBtn.addEventListener('click', openPopupEditProfileBtn);
 profileAddBtn.addEventListener('click', openPopupEditCardBtn);
-popupCloseBtn.forEach(btn => btn.addEventListener('click', closePopupBtn));
+popup.forEach(popup => popup.addEventListener('click', function(evt) {
+  if(evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup__overlay')) {
+    closePopupBtn(evt.target);
+  }
+}));
+popup.forEach(popup => popup.addEventListener('keydown', handleClosePopupKey));
+
+
 profileForm.addEventListener('submit', handleProfileFormSubmit);
+
 cardForm.addEventListener('submit', handleCardFormSubmit);
 
